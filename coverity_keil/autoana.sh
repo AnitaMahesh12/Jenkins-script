0d2d1cc44f45ca84c4768fcd31713278d1076165
AnitaMahesh12 <101692464+AnitaMahesh12@users.noreply.github.com>
2022-04-04 17:03:54 +0530

    Update autoana.sh

--- coverity_keil/autoana.sh
+++ coverity_keil/autoana.sh
@@ -1,3 +1,1 @@
-Welcome to Devops and Enginnering world
-Need to confirm if we make changes in the github repo, changes has to be present in the jenkins job
-Web
+Hi, Welcome to Devops and Enginnering world
